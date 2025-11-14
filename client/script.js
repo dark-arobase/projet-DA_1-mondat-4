a653eb9b828ad19b48a50e8625cc6b056864d716
Gouife A Djiben <e2479537@cmaisonneuve.qc.ca>
2025-11-14 16:03:04 -0500

    debut de serveur

---
+++ client/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('login-form');
+const username= document.getElementById('username');
+const password= document.getElementById('password');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+});
